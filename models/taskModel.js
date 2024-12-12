3a7b4a3f753cfc7b530dc7425c41b7424396eb82
Bhumikamble <bhumikamble714@gmail.com>
2024-12-12 16:37:24 +0530

    updated

---
+++ models/taskModel.js
@@ -0,0 +1,11 @@
+// models/taskModel.js
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String },
+    status: { type: String, default: 'Pending', enum: ['Pending', 'In Progress', 'Completed'] },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Task', taskSchema);
